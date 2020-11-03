cab44825d6e666c9fd0e239ff5f40c06523b8a45
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-11-03 22:30:07 +0000

    Added player
    Added player placeholder
    Added player controller script
    Added camera controller script

--- Space Scavenger/Assets/Scenes/Scripts/PlayerController.cs
+++ Space Scavenger/Assets/Scenes/Scripts/PlayerController.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float horAxis = Input.GetAxis("Horizontal");
+        float verAxis = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horAxis, 0.0f, verAxis);
+
+        GetComponent<Transform>().Translate(movement * speed * Time.deltaTime);
     }
+
+
 }
