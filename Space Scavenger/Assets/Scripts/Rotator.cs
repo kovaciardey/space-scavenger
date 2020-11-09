dbbc1b0bc47f7ffffa72846ec482e5fec9734ef6
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-11-09 22:00:37 +0000

    Miscs
    Dunno.. will probably change most of them again

--- Space Scavenger/Assets/Scripts/Rotator.cs
+++ Space Scavenger/Assets/Scripts/Rotator.cs
@@ -7,6 +7,8 @@ public class Rotator : MonoBehaviour
     public bool monster;
     public bool pickUp;
 
+    public float bobbingThreshold = 0.5f;
+
     // Update is called once per frame
     void Update()
     {
@@ -17,7 +19,17 @@ public class Rotator : MonoBehaviour
         
         if(pickUp)
         {
-            transform.Rotate(new Vector3(0, 45, 30) * Time.deltaTime);
+            transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+
+            float direction = 1;
+
+            transform.Translate(new Vector3(0, 10, 0) * direction * Time.deltaTime);
+
+            if (transform.position.y == bobbingThreshold || transform.position.y == -bobbingThreshold)
+            {
+                direction = -direction;
+            }
         }
     }
 }
+
