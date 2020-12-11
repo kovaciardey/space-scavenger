8be325153713fc40d29fd7e69bafc8a74603eb1d
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-12-11 20:35:43 +0000

    Added Level Generator Basics
    Added Level Generatoe class
    Added Editor class for it, to ahve a button to run from the editor

---
+++ Space Scavenger/Assets/Scripts/LevelGenerator/Editor/LevelGeneratorEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (LevelGenerator))]
+public class LevelGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        LevelGenerator levelGenerator = (LevelGenerator) target;
+
+        //base.OnInspectorGUI();
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate"))
+        {
+            levelGenerator.GenerateLevel();
+        }
+    }
+}
