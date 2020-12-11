298ea966d844dddf16a610979fdb6332362f5843
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-12-11 21:08:00 +0000

    BasicStringGenerator
    Generates a simple string of rooms based on the dictionary provided.

--- Space Scavenger/Assets/Scripts/LevelGenerator/LevelGenerator.cs
+++ Space Scavenger/Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -4,8 +4,41 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
+
+    public int iterations = 4;
+
+    private Dictionary<string, string[]> roomPossibleConnections = new Dictionary<string, string[]> {
+        { "S", new string[2] { "Ra", "Rb" } },
+        { "Ra", new string[3] { "Ra", "Rb", "F" } },
+        { "Rb", new string[3] { "Ra", "Rb", "F" } }
+    };
+
     public void GenerateLevel()
-    {
-        Debug.Log("Generate");
+    { 
+        string levelCode = "S-";
+
+        string currentRoom = levelCode[0].ToString();
+
+        Debug.Log(roomPossibleConnections[currentRoom]);
+
+        for (int i = 0; i < iterations; i++)
+        {
+            Debug.Log(i);
+
+            string[] rooms = roomPossibleConnections[currentRoom];
+
+            string selectedRoom = rooms[Random.Range(0, rooms.Length)];
+
+            levelCode += selectedRoom + "-";
+
+            currentRoom = selectedRoom;
+
+            if (currentRoom == "F")
+            {
+                break;
+            }
+        }
+
+        Debug.Log(levelCode);
     }
 }
