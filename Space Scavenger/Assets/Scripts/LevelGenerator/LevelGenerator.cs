4d501eb8e05d7b977ed84a30ac44735d40cbc7a1
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-12-11 21:44:21 +0000

    Added LevelGen Debug
    Added debug elements for the rooms and materials for each of them

--- Space Scavenger/Assets/Scripts/LevelGenerator/LevelGenerator.cs
+++ Space Scavenger/Assets/Scripts/LevelGenerator/LevelGenerator.cs
@@ -7,19 +7,21 @@ public class LevelGenerator : MonoBehaviour
     public int minLevelLength = 2;
     public int maxLevelLength = 6;
 
+    public Dictionary<string, string> dict = new Dictionary<string, string>();
+
     private string[] selectableRooms = new string[2] { "Ra", "Rb" };
 
     public void GenerateLevel()
     {
         int iterations = Random.Range(minLevelLength, maxLevelLength);
 
-        string levelCode = "S-";
+        string levelCode = "S-C-";
 
         for (int i = 0; i < iterations; i++)
         {
             string room = selectableRooms[Random.Range(0, selectableRooms.Length)];
 
-            levelCode += room + "-";
+            levelCode += room + "-C-";
         }
 
         levelCode += "F";
