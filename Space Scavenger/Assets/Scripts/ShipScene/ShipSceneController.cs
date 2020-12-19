49b4fe50bb18024fb7c24e6dcad915faa09e5fa4
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-12-19 19:20:13 +0000

    Finished Debug
    Finsehd the debug buttons to add scrap and exp manually
    write the level and the scrap to the screen

--- Space Scavenger/Assets/Scripts/ShipScene/ShipSceneController.cs
+++ Space Scavenger/Assets/Scripts/ShipScene/ShipSceneController.cs
@@ -1,18 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShipSceneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int scrap = 1000;
+
+    public int exp = 0;
+    public int maxExp = 100;
+    public int level = 1;
+
+    public Text scrapAmountText;
+    public Text levelText;
+
+    private void Update()
     {
-        
+        ShowScrapAmountText();
+        ShowLevelText();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddScrap(int amount)
     {
-        
+        scrap += amount;
     }
+
+    public void AddExperience(int amount)
+    {
+        if ((exp + amount) == maxExp)
+        {
+            exp = 0;
+            level += 1;
+        }
+        else if ((exp + amount) > maxExp)
+        {
+            int difference = exp + amount - maxExp;
+
+            level += 1;
+
+            exp = difference;
+        }
+        else
+        {
+            exp += amount;
+        }
+    }
+
+    private void ShowScrapAmountText()
+    {
+        scrapAmountText.text = "Scrap: " + scrap;
+    }
+
+    private void ShowLevelText()
+    {
+        levelText.text = level.ToString();
+    } 
 }
