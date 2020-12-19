f8b26ee3b38ca4c60935276b776b5ef47657e486
Andrei Kovaci <andrei.kovaci@gmail.com>
2020-12-19 21:46:01 +0000

    Added the base functionality for the mission selection

---
+++ Space Scavenger/Assets/Scripts/ShipScene/Mission.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mission : MonoBehaviour
+{
+    public Vector2Int shortExpReward;
+    public Vector2Int longExpReward;
+    public Vector2Int shortScrapReward;
+    public Vector2Int longScrapReward;
+
+    private string LevelCode { get; }
+
+    private int ExpReward { get; }
+    private int ScrapReward { get; }
+
+    private string MissionType { get; }
+
+    private int MissionID { get; set; }
+
+    public void GenerateMissionDetails(string missionType, int missionID)
+    {
+
+    }
+
+    private int GenerateExpReward()
+    {
+        if (this.MissionType == "Long")
+        {
+            return Random.Range(longExpReward.x, longExpReward.y);
+        }
+
+        return Random.Range(25, 45);
+    }
+}
